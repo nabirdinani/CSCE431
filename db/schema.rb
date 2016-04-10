27f1188c65c6f373a84818e427dbb99573c62e23
Jason Krezinski <jasonkrez@tamu.edu>
2016-04-09 21:22:53 -0500

    admin panel added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160409220910) do
+ActiveRecord::Schema.define(version: 20160410020929) do
 
   create_table "artworks", force: :cascade do |t|
     t.string   "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20160409220910) do
     t.datetime "activated_at"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
