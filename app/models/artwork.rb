ccc050517ba62abe3dc74448dedc36a3dfc9632d
tafoya77n <tafoya94n@gmail.com>
2016-04-29 23:49:37 +0000

    delete unnecessary  lines from sortby

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -5,29 +5,5 @@ class Artwork < ActiveRecord::Base
   	validates :name, presence: true, length: { maximum: 50 } # Make sure the owner's name is present
   	validates :userid, presence: true
   	
-  	
-  	def self.all_approved_by_order(order)
-        artworks=self.where(:approved=> true)
-        
-        
-        bids =[]
-        case order 
-        when 'highest'
-            artworks.each do |artwork|
-                bid= Bid.where(:artwork_id=> artwork.id).order(amount: :desc).first
-                bids << bid
-            end
-            
-        when 'lowest'
-            ordering = 'lowest'
-        when 'recent'
-            ordering = 'recent'
-        else
-            ordering = 'id'
-            allartworks=artworks
-        end
-        
-        return allartworks
-  end
 
 end
